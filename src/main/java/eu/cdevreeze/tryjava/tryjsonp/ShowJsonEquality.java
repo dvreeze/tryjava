e9dcb88e77f9585d140f7fbfd6bd3bd97e432137
Chris de Vreeze <chris.de.vreeze@caiway.net>
2024-08-30 00:36:10 +0200

    Using SPI type JsonProvider for performance

--- src/main/java/eu/cdevreeze/tryjava/tryjsonp/ShowJsonEquality.java
+++ src/main/java/eu/cdevreeze/tryjava/tryjsonp/ShowJsonEquality.java
@@ -17,6 +17,7 @@
 package eu.cdevreeze.tryjava.tryjsonp;
 
 import jakarta.json.*;
+import jakarta.json.spi.JsonProvider;
 import jakarta.json.stream.JsonGenerator;
 
 import java.io.StringReader;
@@ -29,6 +30,8 @@ import java.util.Map;
  * <p>
  * Note that most static "Json" value creation methods are avoided for performance reasons. See
  * <a href="https://github.com/quarkusio/quarkus/issues/42748">Json-P performance issue</a>.
+ * Going even further, the JsonProvider is looked up once and then re-used all the time, and utility
+ * class Json is avoided entirely.
  *
  * @author Chris de Vreeze
  */
@@ -52,39 +55,38 @@ public class ShowJsonEquality {
                     }""";
 
     public static void main(String[] args) {
-        JsonReaderFactory jsonReaderFactory = Json.createReaderFactory(Map.of());
-        JsonWriterFactory jsonWriterFactory =
-                Json.createWriterFactory(Map.of(JsonGenerator.PRETTY_PRINTING, Boolean.TRUE));
+        // Avoiding expensive Json static methods, that repeatedly look up the JsonProvider
+        JsonProvider jsonProvider = JsonProvider.provider();
+
+        JsonReaderFactory jsonReaderFactory = jsonProvider.createReaderFactory(Map.of());
 
         JsonObject jsonObject = jsonReaderFactory.createReader(new StringReader(JSON_STRING)).readObject();
 
-        var sw = new StringWriter();
-        jsonWriterFactory.createWriter(sw).writeObject(jsonObject);
-        String jsonString = sw.toString();
+        String jsonString = writeJsonObjectToString(jsonObject, jsonProvider);
 
         System.out.printf("Processing JSON (pretty-printed):%n%s%n", jsonString);
 
-        showEqualitiesAndInequalities(jsonObject, jsonWriterFactory);
+        showEqualitiesAndInequalities(jsonObject, jsonProvider);
     }
 
-    public static void showEqualitiesAndInequalities(JsonObject jsonObject, JsonWriterFactory jsonWriterFactory) {
-        showObjectEntryOrderIsIrreleventForEquality(jsonObject, jsonWriterFactory);
-        showArrayElementOrderMattersForEquality(jsonObject, jsonWriterFactory);
-        showUpdatingJsonBreaksEquality(jsonObject, jsonWriterFactory);
-        showAddingDataToJsonBreaksEquality(jsonObject, jsonWriterFactory);
-        showAddingJsonNullToJsonObjectBreaksEquality(jsonObject, jsonWriterFactory);
-        showAddingJsonNullToJsonArrayBreaksEquality(jsonObject, jsonWriterFactory);
-        showRemovingDataFromJsonBreaksEquality(jsonObject, jsonWriterFactory);
-        showRoundtrippingDoesNotBreakEquality(jsonObject, jsonWriterFactory);
-        showChangingObjectEntryOrderEverywhereDoesNotBreakEquality(jsonObject, jsonWriterFactory);
+    public static void showEqualitiesAndInequalities(JsonObject jsonObject, JsonProvider jsonProvider) {
+        showObjectEntryOrderIsIrreleventForEquality(jsonObject, jsonProvider);
+        showArrayElementOrderMattersForEquality(jsonObject, jsonProvider);
+        showUpdatingJsonBreaksEquality(jsonObject, jsonProvider);
+        showAddingDataToJsonBreaksEquality(jsonObject, jsonProvider);
+        showAddingJsonNullToJsonObjectBreaksEquality(jsonObject, jsonProvider);
+        showAddingJsonNullToJsonArrayBreaksEquality(jsonObject, jsonProvider);
+        showRemovingDataFromJsonBreaksEquality(jsonObject, jsonProvider);
+        showRoundtrippingDoesNotBreakEquality(jsonObject, jsonProvider);
+        showChangingObjectEntryOrderEverywhereDoesNotBreakEquality(jsonObject, jsonProvider);
     }
 
-    public static void showObjectEntryOrderIsIrreleventForEquality(JsonObject jsonObject, JsonWriterFactory jsonWriterFactory) {
+    public static void showObjectEntryOrderIsIrreleventForEquality(JsonObject jsonObject, JsonProvider jsonProvider) {
         System.out.println();
         System.out.println("Changing key order in (address) Json object:");
         System.out.println();
 
-        JsonBuilderFactory bf = Json.createBuilderFactory(Map.of());
+        JsonBuilderFactory bf = jsonProvider.createBuilderFactory(Map.of());
 
         JsonObject addressObj = bf.createObjectBuilder()
                 .add("state", "NY")
@@ -93,154 +95,138 @@ public class ShowJsonEquality {
                 .add("streetAddress", "21 2nd Street")
                 .build();
 
-        JsonPatch patch = Json.createPatchBuilder()
+        JsonPatch patch = jsonProvider.createPatchBuilder()
                 .replace("/address", addressObj)
                 .build();
         JsonObject editedObj = patch.apply(jsonObject);
 
-        var sw = new StringWriter();
-        jsonWriterFactory.createWriter(sw).writeObject(editedObj);
-        String jsonString = sw.toString();
+        String jsonString = writeJsonObjectToString(editedObj, jsonProvider);
 
         System.out.printf("Edited Json:%n%s%n", jsonString);
         System.out.println();
         System.out.printf("These Json objects are equal according to Object.equals (expecting true): %b%n", jsonObject.equals(editedObj));
     }
 
-    public static void showArrayElementOrderMattersForEquality(JsonObject jsonObject, JsonWriterFactory jsonWriterFactory) {
+    public static void showArrayElementOrderMattersForEquality(JsonObject jsonObject, JsonProvider jsonProvider) {
         System.out.println();
         System.out.println("Changing order in (phoneNumber) Json array:");
         System.out.println();
 
-        JsonPatch patch = Json.createPatchBuilder()
+        JsonPatch patch = jsonProvider.createPatchBuilder()
                 .move("/phoneNumber/0", "/phoneNumber/1")
                 .build();
         JsonObject editedObj = patch.apply(jsonObject);
 
-        var sw = new StringWriter();
-        jsonWriterFactory.createWriter(sw).writeObject(editedObj);
-        String jsonString = sw.toString();
+        String jsonString = writeJsonObjectToString(editedObj, jsonProvider);
 
         System.out.printf("Edited Json:%n%s%n", jsonString);
         System.out.println();
         System.out.printf("These Json objects are equal according to Object.equals (expecting false): %b%n", jsonObject.equals(editedObj));
     }
 
-    public static void showUpdatingJsonBreaksEquality(JsonObject jsonObject, JsonWriterFactory jsonWriterFactory) {
+    public static void showUpdatingJsonBreaksEquality(JsonObject jsonObject, JsonProvider jsonProvider) {
         System.out.println();
         System.out.println("Updating Json object:");
         System.out.println();
 
-        JsonPatch patch = Json.createPatchBuilder()
+        JsonPatch patch = jsonProvider.createPatchBuilder()
                 .replace("/phoneNumber/0/type", "home phone")
                 .build();
         JsonObject editedObj = patch.apply(jsonObject);
 
-        var sw = new StringWriter();
-        jsonWriterFactory.createWriter(sw).writeObject(editedObj);
-        String jsonString = sw.toString();
+        String jsonString = writeJsonObjectToString(editedObj, jsonProvider);
 
         System.out.printf("Edited Json:%n%s%n", jsonString);
         System.out.println();
         System.out.printf("These Json objects are equal according to Object.equals (expecting false): %b%n", jsonObject.equals(editedObj));
     }
 
-    public static void showAddingDataToJsonBreaksEquality(JsonObject jsonObject, JsonWriterFactory jsonWriterFactory) {
+    public static void showAddingDataToJsonBreaksEquality(JsonObject jsonObject, JsonProvider jsonProvider) {
         System.out.println();
         System.out.println("Adding data to Json object:");
         System.out.println();
 
-        JsonPatch patch = Json.createPatchBuilder()
+        JsonPatch patch = jsonProvider.createPatchBuilder()
                 .add("/phoneNumber/0/description", "Home phone")
                 .build();
         JsonObject editedObj = patch.apply(jsonObject);
 
-        var sw = new StringWriter();
-        jsonWriterFactory.createWriter(sw).writeObject(editedObj);
-        String jsonString = sw.toString();
+        String jsonString = writeJsonObjectToString(editedObj, jsonProvider);
 
         System.out.printf("Edited Json:%n%s%n", jsonString);
         System.out.println();
         System.out.printf("These Json objects are equal according to Object.equals (expecting false): %b%n", jsonObject.equals(editedObj));
     }
 
-    public static void showAddingJsonNullToJsonObjectBreaksEquality(JsonObject jsonObject, JsonWriterFactory jsonWriterFactory) {
+    public static void showAddingJsonNullToJsonObjectBreaksEquality(JsonObject jsonObject, JsonProvider jsonProvider) {
         System.out.println();
         System.out.println("Adding Json null to (Json object in) Json object:");
         System.out.println();
 
-        JsonPatch patch = Json.createPatchBuilder()
+        JsonPatch patch = jsonProvider.createPatchBuilder()
                 .add("/phoneNumber/0/description", JsonValue.NULL)
                 .build();
         JsonObject editedObj = patch.apply(jsonObject);
 
-        var sw = new StringWriter();
-        jsonWriterFactory.createWriter(sw).writeObject(editedObj);
-        String jsonString = sw.toString();
+        String jsonString = writeJsonObjectToString(editedObj, jsonProvider);
 
         System.out.printf("Edited Json:%n%s%n", jsonString);
         System.out.println();
         System.out.printf("These Json objects are equal according to Object.equals (expecting false): %b%n", jsonObject.equals(editedObj));
     }
 
-    public static void showAddingJsonNullToJsonArrayBreaksEquality(JsonObject jsonObject, JsonWriterFactory jsonWriterFactory) {
+    public static void showAddingJsonNullToJsonArrayBreaksEquality(JsonObject jsonObject, JsonProvider jsonProvider) {
         System.out.println();
         System.out.println("Adding Json null to (Json array in) Json object:");
         System.out.println();
 
-        JsonPatch patch = Json.createPatchBuilder()
+        JsonPatch patch = jsonProvider.createPatchBuilder()
                 .add("/phoneNumber/2", JsonValue.NULL)
                 .build();
         JsonObject editedObj = patch.apply(jsonObject);
 
-        var sw = new StringWriter();
-        jsonWriterFactory.createWriter(sw).writeObject(editedObj);
-        String jsonString = sw.toString();
+        String jsonString = writeJsonObjectToString(editedObj, jsonProvider);
 
         System.out.printf("Edited Json:%n%s%n", jsonString);
         System.out.println();
         System.out.printf("These Json objects are equal according to Object.equals (expecting false): %b%n", jsonObject.equals(editedObj));
     }
 
-    public static void showRemovingDataFromJsonBreaksEquality(JsonObject jsonObject, JsonWriterFactory jsonWriterFactory) {
+    public static void showRemovingDataFromJsonBreaksEquality(JsonObject jsonObject, JsonProvider jsonProvider) {
         System.out.println();
         System.out.println("Removing data from Json object:");
         System.out.println();
 
-        JsonPatch patch = Json.createPatchBuilder()
+        JsonPatch patch = jsonProvider.createPatchBuilder()
                 .remove("/phoneNumber/1/type")
                 .build();
         JsonObject editedObj = patch.apply(jsonObject);
 
-        var sw = new StringWriter();
-        jsonWriterFactory.createWriter(sw).writeObject(editedObj);
-        String jsonString = sw.toString();
+        String jsonString = writeJsonObjectToString(editedObj, jsonProvider);
 
         System.out.printf("Edited Json:%n%s%n", jsonString);
         System.out.println();
         System.out.printf("These Json objects are equal according to Object.equals (expecting false): %b%n", jsonObject.equals(editedObj));
     }
 
-    public static void showRoundtrippingDoesNotBreakEquality(JsonObject jsonObject, JsonWriterFactory jsonWriterFactory) {
+    public static void showRoundtrippingDoesNotBreakEquality(JsonObject jsonObject, JsonProvider jsonProvider) {
         System.out.println();
         System.out.println("Roundtripping (writing and parsing back again):");
         System.out.println();
 
-        var sw = new StringWriter();
-        jsonWriterFactory.createWriter(sw).writeObject(jsonObject);
-        String jsonString = sw.toString();
+        String jsonString = writeJsonObjectToString(jsonObject, jsonProvider);
 
-        JsonObject jsonObj2 = Json.createReader(new StringReader(jsonString)).readObject();
+        JsonObject jsonObj2 = jsonProvider.createReader(new StringReader(jsonString)).readObject();
 
         System.out.printf("These Json objects are equal according to Object.equals (expecting true): %b%n", jsonObject.equals(jsonObj2));
     }
 
-    public static void showChangingObjectEntryOrderEverywhereDoesNotBreakEquality(JsonObject jsonObject, JsonWriterFactory jsonWriterFactory) {
+    public static void showChangingObjectEntryOrderEverywhereDoesNotBreakEquality(JsonObject jsonObject, JsonProvider jsonProvider) {
         System.out.println();
         System.out.println("Changing key order everywhere in Json object (but not in Json arrays):");
         System.out.println();
 
-        JsonBuilderFactory bf = Json.createBuilderFactory(Map.of());
+        JsonBuilderFactory bf = jsonProvider.createBuilderFactory(Map.of());
 
         JsonObject jsonObj2 = bf.createObjectBuilder()
                 .add("address", bf.createObjectBuilder()
@@ -264,12 +250,19 @@ public class ShowJsonEquality {
                 .add("firstName", "John")
                 .build();
 
-        var sw = new StringWriter();
-        jsonWriterFactory.createWriter(sw).writeObject(jsonObj2);
-        String jsonString = sw.toString();
+        String jsonString = writeJsonObjectToString(jsonObj2, jsonProvider);
 
         System.out.printf("Edited Json:%n%s%n", jsonString);
         System.out.println();
         System.out.printf("These Json objects are equal according to Object.equals (expecting true): %b%n", jsonObject.equals(jsonObj2));
+    }
+
+    private static String writeJsonObjectToString(JsonObject jsonObj, JsonProvider jsonProvider) {
+        JsonWriterFactory jsonWriterFactory =
+                jsonProvider.createWriterFactory(Map.of(JsonGenerator.PRETTY_PRINTING, Boolean.TRUE));
+
+        var sw = new StringWriter();
+        jsonWriterFactory.createWriter(sw).writeObject(jsonObj);
+        return sw.toString();
     }
 }
